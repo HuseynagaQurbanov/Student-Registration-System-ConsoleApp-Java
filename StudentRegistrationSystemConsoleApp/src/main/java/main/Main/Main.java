38bfe36b263f1ef483844444d225563409da123f
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2024-01-29 18:00:26 +0400

    Initial Commit

---
+++ StudentRegistrationSystemConsoleApp/src/main/java/main/Main/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main.Main;
+
+/**
+ *
+ * @author SOFT
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi there");
+    }
+}
